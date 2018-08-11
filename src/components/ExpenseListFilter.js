2f450156acc4a66810b59443eec76d253d6b3e08
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-08-11 17:47:55 -0400

    added sortby field

--- src/components/ExpenseListFilter.js
+++ src/components/ExpenseListFilter.js
@@ -1,13 +1,22 @@
 import { connect } from "react-redux";
-import { setTextFilter } from "../actions/filters";
+import { setTextFilter, sortByDate, sortByAmount } from "../actions/filters";
 
 import React from "react";
 const ExpenseListFilter = (props) => (
   <div>
     <input type="text" value={props.filters.text} onChange={(e) => {
       props.dispatch(setTextFilter(e.target.value))
-      console.log(e.target.value)
     }}/>
+    <select onChange={(e) => {
+      if(e.target.value === "date"){
+        props.dispatch(sortByDate());
+      } else {
+        props.dispatch(sortByAmount());
+      }
+    }}>
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
   </div>
 );
 
