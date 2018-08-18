9e1098ddc7b3f8ff7151b28dc56eeee9deea1dbd
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-08-17 22:52:39 -0400

    setting up jest + enzyme

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,12 +1,18 @@
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow"; // Shallow rendering is for specific components...
 import Header from "../../components/Header";
+import { shallow } from "enzyme";
+import toJSON from "enzyme-to-json";
+
+// React shallow renderer (worse method)
+  // import ReactShallowRenderer from "react-test-renderer/shallow"; // Shallow rendering is for specific components...
+  /*test("Should render header correctly", () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+  });*/
 
 test("Should render header correctly", () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  // console.log(renderer.getRenderOutput());
-
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
-
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
 });
+
