ebe0db5a7f0d3cba5e8ef8a9980cccb796cb693a
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-08-24 12:33:46 -0400

    added firebase connection to add expenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,18 +1,13 @@
 import uuid from "uuid";
 import moment from "moment";
+import database from "../firebase/firebase";
 
 // Action Generators
   // ADD_EXPENSE action generator
-   export const addExpense = ({ description = "", note = "", amount = 0, createdAt = moment().valueOf() } = {}) => ({
+   export const addExpense = (expense) => ({
       type: "ADD_EXPENSE",
-      expense: {
-        id: uuid(), // uuid is temporary alternative for id, will come from mongodb later
-        description,
-        note,
-        amount,
-        createdAt
-      }
-    })
+      expense
+    });
   // REMOVE_EXPENSE action generator
    export const removeExpense = ({ id } = {}) => ({
       type: "REMOVE_EXPENSE",
@@ -23,4 +18,19 @@ import moment from "moment";
       type: "EDIT_EXPENSE",
       id,
       updates
-    })
+    });
+
+   // This is made possible by our Redux middleware, thunk.
+   // Thunk allows us to "dispatch" functions. The function connects to firebase, and then returns our action...
+
+   export const startAddExpense = ({ description = "", note = "", amount = 0, createdAt = moment().valueOf() } = {}) => {
+    return (dispatch) => { // The
+      const expense = { description, note, amount, createdAt };
+      database.ref("expenses").push(expense).then((ref) => {
+         dispatch(addExpense({
+          id: ref.key, // from firebase...
+          ...expense
+         }));
+      });
+    };
+   };
