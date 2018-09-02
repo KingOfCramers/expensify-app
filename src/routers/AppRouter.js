94627ff389639e7ab2e7e92e3133ce4aab439795
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-09-02 18:24:53 -0400

    added PublicRoute for '/' login route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom'; // Only use Route for public routes...
 import PrivateRoute from "./PrivateRoute"; // HOC of Route.
+import PublicRoute from "./PublicRoute"; // HOC of Route.
 
 // History
 import createHistory from "history/createBrowserHistory";
@@ -11,17 +12,17 @@ export const history = createHistory();
 import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
 import AddExpensePage from "../components/AddExpensePage";
 import EditExpensePage from "../components/EditExpensePage";
-import HelpPage from "../components/HelpPage";
 import EditPage from "../components/EditExpensePage";
 import FourOhFour from "../components/FourOhFour";
 import Login from "../components/Login";
+import HelpPage from "../components/HelpPage";
 
 
 const AppRouter = () => (
   <Router history={history} >
     <div>
       <Switch>
-        <Route path="/" component={Login} exact={true} />
+        <PublicRoute path="/" component={Login} exact={true} />
         <PrivateRoute exact={true} path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
